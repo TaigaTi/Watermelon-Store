33064ab973ee1b9b5ff48eaed1b31036d5e8d857
TaigaTi <tarikabirch@gmail.com>
2023-08-11 16:49:11 -0400

    add cart subtotal component

--- watermelon-ui/src/app/cart-sub/cart-sub.component.ts
+++ watermelon-ui/src/app/cart-sub/cart-sub.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { Product } from '../entities/product-entity';
 
 @Component({
   selector: 'app-cart-sub',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-sub.component.css']
 })
 export class CartSubComponent {
+  items: Product[] = [];
+  subtotal:number = 0;
 
+  constructor(private cartService:CartService){}
+
+  ngOnInit(){
+
+    this.items = this.cartService.getItems();
+
+    for(let i=0;i<this.items.length;i++)
+    {
+      this.subtotal += this.items[i].price;
+    }
+  }
 }
